db4dda2b96921be5a688e1bf3e8ee0bb4541a52e
Afraaz <ahmedalif102@gmail.com>
2022-10-11 22:20:09 +0600

    statistics-added

--- src/Components/Stats.jsx
+++ src/Components/Stats.jsx
@@ -1,9 +1,40 @@
 import React from 'react';
-
+import { LineChart, Line, CartesianGrid, XAxis, YAxis } from "recharts";
 const Stats = () => {
+  const data = [
+    {
+      id: 1,
+      name: "React",
+      logo: "https://live.staticflickr.com/65535/52413593240_e00326e727_o.png",
+      total: 8,
+    },
+    {
+      id: 2,
+      name: "JavaScript",
+      logo: "https://live.staticflickr.com/65535/52412638962_12e932256a_o.png",
+      total: 9,
+    },
+    {
+      id: 4,
+      name: "CSS",
+      logo: "https://live.staticflickr.com/65535/52413665713_5977a693cb_o.png",
+      total: 8,
+    },
+    {
+      id: 5,
+      name: "Git",
+      logo: "https://live.staticflickr.com/65535/52412639027_5692c15b3f_o.png",
+      total: 11,
+    },
+  ];
   return (
-    <div>
-      
+    <div className='flex justify-center mt-11'>
+      <LineChart width={600} height={300} data={data}>
+        <Line type="monotone" dataKey="total" stroke="#8884d8" />
+        <CartesianGrid stroke="#ccc" />
+        <XAxis dataKey="name" />
+        <YAxis />
+      </LineChart>
     </div>
   );
 };
