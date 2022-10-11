383f32ca2ff3959dc4672ff96a98a24eab343268
Afraaz <ahmedalif102@gmail.com>
2022-10-11 19:33:23 +0600

    some-changes

--- src/Components/TestOfCourses.jsx
+++ src/Components/TestOfCourses.jsx
@@ -7,12 +7,12 @@ const TestOfCourses = ({ ts }) => {
   console.log(ts);
   let correctAnswers;
   const handleEyeBtn = (correctAnswer) => {
-    correctAnswers = `Wow correct Ans is............${correctAnswer}`;
+    correctAnswers = `Correct Ans is............${correctAnswer}`;
     toast.success(correctAnswers)
   };
   const { options, question, correctAnswer, id } = ts;
   return (
-    <div className="border border-red-900 mt-4 h-[350px] p-3 rounded-md ml-2 mr-2">
+    <div className="border border-red-900 mt-4 h-[440px] p-3 rounded-md ml-2 mr-2">
       <div className="flex justify-between">
         <h2 className="text-lg font-semibold text-fuchsia-600">
           Question : {question}
@@ -24,7 +24,7 @@ const TestOfCourses = ({ ts }) => {
         ></FontAwesomeIcon>
       </div>
       <div className="grid grid-cols-2 gap-4 mt-[50px]">
-        <div className="bg-slate-300 border border-slate-700 h-[90px] flex items-center rounded mt-2">
+        <div className="bg-slate-300 border border-slate-700 h-[80px] flex items-center rounded mt-2">
           <input className="ml-2" type="radio" name="opt" id="1" /> {options[0]}
         </div>
         <div className="bg-slate-300 border border-slate-700 h-[90px] flex items-center rounded mt-2">
