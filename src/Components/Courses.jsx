041a0e073da4bff1d8a1072d9097df9779f3ec5c
Afraaz <ahmedalif102@gmail.com>
2022-10-11 13:23:26 +0600

    courses-header-done

--- src/Components/Courses.jsx
+++ src/Components/Courses.jsx
@@ -1,9 +1,12 @@
 import React from 'react';
-
+import banner from '../banner/2.png'
 const Courses = () => {
   return (
-    <div>
-      
+    <div className='mt-2'>
+      <div>
+        <h2 className="text-3xl mb-4 text-center text-lime-600 font-bold">We quiz therefore we are.</h2>
+      </div>
+      <img className="mx-auto w-[70%] h-[300px]" src={banner} alt="" />
     </div>
   );
 };
