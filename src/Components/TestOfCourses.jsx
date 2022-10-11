b0109ae99d0c1319499296c5903ce9942f8fef2b
Afraaz <ahmedalif102@gmail.com>
2022-10-11 19:16:12 +0600

    toast-added-on-eye-btn

--- src/Components/TestOfCourses.jsx
+++ src/Components/TestOfCourses.jsx
@@ -1,14 +1,24 @@
 import React from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faEye } from "@fortawesome/free-solid-svg-icons";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 const TestOfCourses = ({ ts }) => {
   console.log(ts);
+  let correctAnswers;
+  const handleEyeBtn = (correctAnswer) => {
+    correctAnswers = `Wow correct Ans is............${correctAnswer}`;
+    toast.success(correctAnswers)
+  };
   const { options, question, correctAnswer, id } = ts;
   return (
     <div className="border border-red-900 mt-4 h-[350px] p-3 rounded-md ml-2 mr-2">
       <div className="flex justify-between">
-        <h2 className="text-lg font-semibold text-fuchsia-600">Question : {question}</h2>
+        <h2 className="text-lg font-semibold text-fuchsia-600">
+          Question : {question}
+        </h2>
         <FontAwesomeIcon
+          onClick={() => handleEyeBtn(correctAnswer)}
           className="text-green-600 "
           icon={faEye}
         ></FontAwesomeIcon>
@@ -27,6 +37,9 @@ const TestOfCourses = ({ ts }) => {
           <input className="ml-2" type="radio" name="opt" id="4" /> {options[3]}
         </div>
       </div>
+      <ToastContainer
+      position="top-center"
+      />
     </div>
   );
 };
